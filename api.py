ec9f96618c2e969121d7d1b6411844b1ff1bd0c6
Joshua Ludolf <Jludo01@jaguar.tamu.edu>
2024-01-05 17:53:16 -0600

    UPDATE

--- api.py
+++ api.py
@@ -1,23 +1,25 @@
 # To run open terminal and type the following:
 # uvicorn API:app --reload
 
-import product
+import Product
 from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 inventory = {
-    1: product.Product('Whole Milk', 3.99, 'Borden').__str__,
-    2: product.Product('Fat Free Milk', 4.99, 'Borden').__str__
+    1: Product.Product('Whole Milk', 3.99, 'Borden').__str__,
+    2: Product.Product('Fat Free Milk', 4.99, 'Borden').__str__
 }
+
+inventory_list = []
+
+for key in inventory:
+    inventory_list.append(inventory[key])
 
 
 @app.get("/")
 def home():
-    inventory_list = ''
-    for key, i in inventory.items():
-        inventory_list += i + '\n'
-    return f'{inventory_list}'
+    return {'Inventory': f'{inventory_list}'}  # JSON format = python dictionary
 
 
 @app.get('/get-item/{item_id}')
